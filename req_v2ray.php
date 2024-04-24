bb8ae18fd3d1f361d892c34e37ed32c541f8b660
Husswin <124583657+Husswin@users.noreply.github.com>
2024-04-24 19:09:37 +0330

    Update req_v2ray.php

--- req_v2ray.php
+++ req_v2ray.php
@@ -3,38 +3,39 @@
 #source = https://t.me/Learn_launch
 #Author: @Drstripe
 
-$url = 'https://kabiriteam.nl.subhost.site/DrApi/';
-$token = 'Token';  #Put your token that you received from the @Authyourbot 
+$url = 'https://kabiriteam.nl.subhost.site/Api';
+$data = [
+    'number' => '09353947285',
+    #It is better not to change this, but if you change it, there will be no problem👇🏻
+    'жетон' => 'Zv9EakふーんXuV7+_!ks)_!nyBG3p', 
+    'token' => 'PtzPAIIUqngJ5vaHM49apfxQobhKLy9EjgmBVurk7G6WjGFDvuNYSxAyO10E8otq8DKki' // Put your token that you received from the @authorizeu_bot
+];
+$headers = [
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Connection: keep-alive'
+];
 
-$data = array('CW914I23INIS152K0DFMOOOWQ5NA7QMD7N9134T7V5K674062NHPKC9XMP2GZ3QHXK249' => $token);
-
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_POST, 1);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-if ($httpCode == 200) {
-    $server_info = json_decode($response, true);
-    echo "Server info received successfully:\n";
-    print_r($server_info);
+if ($response === false) {
+    echo "Error sending request to server or invalid Token";
 } else {
-    if ($httpCode == 400) {
-        echo "Bad request. Please check your data and try again.\n";
-    } elseif ($httpCode == 401) {
-        echo "Unauthorized. Please check your token.\n";
-    } elseif ($httpCode == 404) {
-        echo "Not found. The requested resource was not found on the server.\n";
-    } elseif ($httpCode == 405) {
-        echo "Method not allowed. Please check the HTTP method.\n";
-    } elseif ($httpCode == 500) {
-        echo "Internal server error. Please try again later.\n";
+    $responseData = json_decode($response, true);
+    if ($responseData !== null) {
+        echo json_encode($responseData, JSON_UNESCAPED_UNICODE);
     } else {
-        echo "An error occurred. HTTP Code: $httpCode\n";
+        echo "Invalid JSON response from server";
     }
 }
+
 #source = https://t.me/Learn_launch
 #Author: @Drstripe
 
